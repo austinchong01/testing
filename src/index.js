cd91176eb99f0dfc186689ad3621a0f32fd61bc9
Austin <austin.inhyuk.chong@gmail.com>
2025-04-11 11:51:08 -0700

    Finish Testing Caesar Cipher

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-import "./styles.css";
+// import "./styles.css";
 
-console.log("TEST")
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
+
+const calculator = {
+  add(one, two) {
+    return one + two;
+  },
+  sub(one, two) {
+    return one - two;
+  },
+  div(one, two) {
+    return one / two;
+  },
+  mult(one, two) {
+    return one * two;
+  },
+};
+
+function caesarCipher(string, key) {
+  /*
+    ASCII Values
+
+    Lower Case: 97(a) - 122(z)
+    Upper Case: 65(A) - 90(Z)
+    */
+
+  const res = [];
+  for (let i = 0; i < string.length; i += 1) {
+    const asciiCode = string.charCodeAt(i);
+    if (
+      !(
+        (asciiCode >= 97 && asciiCode <= 122) ||
+        (asciiCode >= 65 && asciiCode <= 90)
+      )
+    ) {
+      res.push(String.fromCharCode(asciiCode));
+      continue;
+    }
+
+    let offset = asciiCode + key;
+    if (
+      (offset > 122 && asciiCode >= 97 && asciiCode <= 122) ||
+      (offset > 90 && asciiCode >= 65 && asciiCode <= 90)
+    )
+      offset -= 26;
+    res.push(String.fromCharCode(offset));
+  }
+  return res.join("");
+}
+
+module.exports = { capitalize, reverseString, calculator, caesarCipher };
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,29 @@
-const sum = require('./sum');
+const {capitalize, reverseString, calculator, caesarCipher} = require('./index');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('capitalize', () => {
+  expect(capitalize("aweston")).toBe("Aweston");
+  expect(capitalize("test")).toBe("Test");
+  expect(capitalize("test")).not.toBe("test");
+  expect(capitalize("ANY")).toBeDefined();
+});
+
+test("reverse string", () => {
+  expect(reverseString("aweston")).toBe("notsewa");
+  expect(reverseString("test")).toBe("tset");
+  expect(reverseString("test")).not.toBe("test");
+  expect(reverseString("ANY")).toBeDefined();
+});
+
+test("calculator", () => {
+  expect(calculator.add(2, 2)).toBe(4);
+  expect(calculator.sub(9, 2)).toBe(7);
+  expect(calculator.div(33, 11)).toBe(3);
+  expect(calculator.mult(3, 8)).toBe(24);
+});
+
+test("caesar cipher", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+  expect(caesarCipher("xyz", 1)).toBe("yza");
+  expect(caesarCipher("aweston", 4)).toBe("eaiwxsr");
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
 });
